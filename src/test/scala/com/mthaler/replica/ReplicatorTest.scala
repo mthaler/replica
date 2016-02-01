9bebace7d2c719abb97b6a58a0082129de773a0b
Michael Thaler <michael.thaler@dlr.de>
2016-02-01 15:55:27 +0100

    Added test for Replicator.replicate.

--- src/test/scala/com/mthaler/replica/ReplicatorTest.scala
+++ src/test/scala/com/mthaler/replica/ReplicatorTest.scala
@@ -2,9 +2,19 @@ package com.mthaler.replica
 
 import org.scalatest.FunSuite
 
+object ReplicatorTest {
+  case class Person(name: String, age: Int)
+}
+
 class ReplicatorTest extends FunSuite {
 
-  test("replicate") {
+  import ReplicatorTest._
 
+  test("replicate") {
+    val p = Person("Richard Feynman", 42)
+
+    assertResult(p) {
+      Replicator.replicate(p)
+    }
   }
 }
