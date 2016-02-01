61d243e8655bbeecfa22246e23d47b0baba5d433
Michael Thaler <michael.thaler@dlr.de>
2016-02-01 16:01:20 +0100

    Create copy of class using reflection.

--- src/main/scala/com/mthaler/replica/Replicator.scala
+++ src/main/scala/com/mthaler/replica/Replicator.scala
@@ -2,5 +2,9 @@ package com.mthaler.replica
 
 object Replicator {
 
-  def replicate[T <: Product](t: T): T = t
+  def replicate[T <: Product](t: T): T = {
+    val values = t.productIterator.toList.map(_.asInstanceOf[AnyRef])
+    val result = t.getClass.getConstructors.head.newInstance(values: _*).asInstanceOf[T]
+    result
+  }
 }
