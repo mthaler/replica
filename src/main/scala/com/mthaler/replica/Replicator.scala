d8202ae6c406574ca655f36e2b5c583cea59137a
Michael Thaler <michael.thaler@dlr.de>
2016-02-02 16:27:55 +0100

    Added unwrap method that can unwrap value classes.

--- src/main/scala/com/mthaler/replica/Replicator.scala
+++ src/main/scala/com/mthaler/replica/Replicator.scala
@@ -11,12 +11,23 @@ object Replicator {
    * @return copy of case class with updated values
    */
   def copy[T <: Product](t: T, updated: Map[String, Any] = Map.empty[String, Any]): T = {
+    // create list of values of the class
     val values = t.productIterator.toList.map(_.asInstanceOf[AnyRef])
     val clazz = t.getClass
+    // create list of fields of the class
     val fields = clazz.getDeclaredFields.toList
     val fieldNames = fields.map(_.getName)
     val newValues = for ((field, value) <- fieldNames zip values) yield if (updated.contains(field)) updated(field).asInstanceOf[AnyRef] else value
     val result = clazz.getConstructors.head.newInstance(newValues: _*).asInstanceOf[T]
     result
+  }
+
+  private[replica] def unwrap(value: Any): Any = {
+    val clazz = value.getClass
+    val fields = clazz.getDeclaredFields.toList
+    if (fields.size != 1) throw new IllegalArgumentException("Not a value class, value classes must have exactly one field!")
+    val f = fields.head
+    f.setAccessible(true)
+    f.get(value)
   }
 }
--- src/test/scala/com/mthaler/replica/ReplicatorTest.scala
+++ src/test/scala/com/mthaler/replica/ReplicatorTest.scala
@@ -9,6 +9,9 @@ object ReplicatorTest {
   }
   case class Person(name: String, age: Int) extends HasName
   case class Pet(name: String) extends HasName
+  case class Test(a: Boolean, b: String, c: Person)
+  case class Time(val ticks: Long) extends AnyVal
+  case class Test2(enabled: Boolean, time: Time)
 }
 
 class ReplicatorTest extends FunSuite {
@@ -30,5 +33,27 @@ class ReplicatorTest extends FunSuite {
     }
     val p = Person("Richard Feynman", 42).withName("Paul Dirac")
     println(p)
+  }
+
+  test("complexClass") {
+    implicit class RichTest(value: Test) {
+      def withBoolean(b: Boolean) = Replicator.copy(value, Map("a" -> b))
+    }
+    val test = Test(true, "test", Person("Richard Feynman", 42))
+  }
+
+  test("valueClass") {
+    implicit class RichTest(value: Test2) {
+      def withBoolean(b: Boolean) = Replicator.copy(value, Map("enabled" -> b))
+    }
+    val test = Test2(true, Time(0))
+    println(test.withBoolean(false))
+  }
+
+  test("unwrap") {
+    assertResult(0l) {
+      val t = Time(0)
+      Replicator.unwrap(t)
+    }
   }
 }
